4e0282c70af3b4ead116409d3ff438f7cd704ed6
mitsu1119 <hachimitsu1119@gmail.com>
2019-07-09 20:03:34 +0900

    Enemyのスコア計算でのバグを修正

--- src/field.cpp
+++ src/field.cpp
@@ -183,35 +183,35 @@ UF Field::makePureTreeMine() {
 }
 
 UF Field::makePureTreeEnemy() {
-	UF pureTree(field.size());
+    UF pureTree(field.size());
 
-	for(size_t i = 0; i < height; i++) {
-		for(size_t j = 0; j < width; j++) {
-			if(at(j,i)->isEnemyPanel()) continue;
-			// UP
-			if(i - 1 >= 0) {
-				if(!at(j, i - 1)->isEnemyPanel()) {
-					pureTree.unite(xyIndex(j, i), xyIndex(j, i - 1));
-				}
-			}
-			// DOWN
-			if(i + 1 < height) {
-				if(!at(j, i + 1)->isEnemyPanel()) {
-					pureTree.unite(xyIndex(j, i), xyIndex(j, i + 1));
-				}
-			}
-			// RIGHT
-			if(j + 1 < width) {
-				if(!at(j + 1, i)->isEnemyPanel()) {
-					pureTree.unite(xyIndex(j, i), xyIndex(j + 1, i));
-				}
-			}
-			// LEFT
-			if(j - 1 >= 0) {
-				if(!at(j - 1, i)->isEnemyPanel()) {
-					pureTree.unite(xyIndex(j, i), xyIndex(j - 1, i));
-				}
-			}
+    for(int i=0; i<height; i++) {
+        for(int j=0; j<width; j++) {
+            if(!this->at(j,i)->isPurePanel() && !this->at(j,i)->isMyPanel()) continue;
+    		    // UP
+                if(i-1 >= 0) {
+                        if(this->at(j, i-1)->isPurePanel() || this->at(j, i-1)->isMyPanel()) {
+                                pureTree.unite(xyIndex(j,i), xyIndex(j,i-1));
+                        }
+                }
+                // DOWN
+                if(i+1 < height) {
+                        if(this->at(j, i+1)->isPurePanel() || this->at(j, i+1)->isMyPanel()) {
+                                pureTree.unite(xyIndex(j,i), xyIndex(j,i+1));
+                        }
+                }
+                // RIGHT
+                if(j+1 < width) {
+                        if(this->at(j+1, i)->isPurePanel() || this->at(j+1, i)->isMyPanel()) {
+                                pureTree.unite(xyIndex(j,i), xyIndex(j+1, i));
+                        }
+                }
+                // LEFT
+                if(j-1 >= 0) {
+                        if(this->at(j-1, i)->isPurePanel()  || this->at(j-1, i)->isMyPanel()) {
+                                pureTree.unite(xyIndex(j,i), xyIndex(j-1, i));
+                        }
+                }
 		}
 	}
 	return pureTree;
